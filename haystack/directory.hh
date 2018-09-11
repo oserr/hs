ac4061144c94fa5155a35fe04303c26b7b3b0930
Omar Serrano <oser520@gmail.com>
2018-09-11 16:14:24 -0400

    Add variable and class header comments for Directory

--- haystack/directory.hh
+++ haystack/directory.hh
@@ -9,26 +9,50 @@
 #include <bsoncxx/json.hpp>
 #include <mongocxx/instance.hpp>
 
+/**
+ * The directory component.
+ *
+ * Maps needle IDs to stores. It can receive commands to upload blobs, list the
+ * IDs of the blobs in the stores, and to remove a blob. When it receives an
+ * upload request, it allocates a needle ID for the blob, chooses a store where
+ * to put the blob, and forwards the upload command to the chosen store. On a
+ * list command, it simply responds with all the needle IDs in the stores. On a
+ * remove command, the blob is not removed from the store, but the needle ID is
+ * no longer included with the other needle IDs when a needle ID list is
+ * provided.
+ *
+ * The directory uses MongoDB to store the IDs.
+ */
 class Directory
 {
 public:
     using TcpStream = boost::asio::ip::tcp::iostream;
 
 private:
+    // The MongoDB database and collection names.
     static constexpr char const *kDbName = "HAYSTACK";
     static constexpr char const *kDbCollectionName = "NEEDLES";
+
+    // The number of volumes in the store.
     static constexpr unsigned kVolumes = 5;
     static constexpr uint64_t kMaxFileSize = 1 << 20;
 
+    // The IP address and port where Directory listens for requests.
     std::string dirIpAddr;
     unsigned dirPort;
+
+    // The MongoDB URI.
     std::string mongoUri;
+
+    // The IP address and port where Store listens for requests.
     std::string storeIpAddr;
     std::string storePort;
 
+    // Numbers to keep track of the volume number and ID for the next needle.
     std::atomic_int volumeCounter;
     std::atomic_llong idCounter;
 
+    // The MongoDB instance.
     mongocxx::instance mongoInstance;
 
     void HandleConnection(std::unique_ptr<TcpStream> conn);
