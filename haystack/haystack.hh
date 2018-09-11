f65a87d95346eb9419ca9635f2a485472361944c
Omar Serrano <oser520@gmail.com>
2018-09-11 16:27:21 -0400

    Add class header comment for Haystack

--- haystack/haystack.hh
+++ haystack/haystack.hh
@@ -8,12 +8,14 @@
 
 #include "needle.hh"
 
+// Haystack errors.
 enum class HsErr
 {
     BadNeedle,
     NoFit
 };
 
+// Haystack exception, which will contain an HsErr code.
 class HaystackErr : std::exception
 {
     HsErr err;
@@ -39,6 +41,17 @@ public:
     }
 };
 
+/**
+ * The Haystack component.
+ *
+ * It organizes multiple needles in a single file. The file is organized as an
+ * array of needles. Each needle has an ID, a size attribute, a deleted status
+ * flag attribute, and a binary blob attribute. The first three are fixed size,
+ * the blob is variable, and the size attribute indicates the size of the blob
+ * in terms of bytes. Thus, the information in each Haystack file is self
+ * contained because everything, such as the number of needles, which are
+ * deleted, and so on, can be reconstructed by simply traversing the file.
+ */
 class Haystack
 {
     mutable std::mutex mtx;  // To protect writing/reading to the file.
